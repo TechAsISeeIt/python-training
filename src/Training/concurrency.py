bbc8d0b2a114115505d5c0e7cab864a864fcdf87
udaydoppalapudi <reachudayd@gmail.com>
2026-03-12 09:32:48 +0530

    Pandas Examples Added

--- src/Training/concurrency.py
+++ src/Training/concurrency.py
@@ -1,5 +1,6 @@
 import asyncio
 import httpx
+from datetime import datetime
 
 urls = [
     "https://httpbin.org/get",
@@ -9,19 +10,28 @@ urls = [
 ]
 
 
-async def fetch(client, url):
+async def fetch(client: httpx.AsyncClient, url: str) -> httpx.Response:
     response = await client.get(url)
     print(f"{url} -> {response.status_code}")
     return response
 
 
+async def normal():
+    print(datetime.now())
+    async with httpx.AsyncClient() as client:
+        for url in urls:
+            print(await fetch(client, url))
+    print(datetime.now())
+
+
 async def main():
+    print(datetime.now())
     async with httpx.AsyncClient() as client:
         tasks = [fetch(client, url) for url in urls]
-        results = await asyncio.gather(*tasks)
-        for r in results:
-            print(r.json())
+        results = await asyncio.gather(*tasks)  # Registering ASync Tasks
+        print(results)
+    print(datetime.now())
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(normal())
